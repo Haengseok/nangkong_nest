04c92bbba3c64c26a614baa08f2a04b975712309
hangkong <godtjrdl98@gmail.com>
2023-12-27 16:20:49 +0900

    docker 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { GraphQLModule } from '@nestjs/graphql';
+import { databaseConfig } from './config/database/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    // GraphQLModule.forRoot({
+    //   autoSchemaFile: true,
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
