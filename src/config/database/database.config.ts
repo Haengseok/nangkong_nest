5f637204eb4938629e4a02472c7a63d32726a5e2
nangkong_ion <godtjrdl98@gmail.com>
2024-02-19 18:55:01 +0900

    mysql 추가

--- src/config/database/database.config.ts
+++ src/config/database/database.config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 3306,
+  port: parseInt(process.env.MYSQL_PORT, 10) || 3306,
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_DATABASE || 'my_database',
