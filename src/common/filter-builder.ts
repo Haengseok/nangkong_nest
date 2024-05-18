8ef726a4c3afdf1ee3e652b440b42dd699e749df
“nangkong” <godtjrdl98@gmail.com>
2024-05-18 16:14:50 +0900

    filter 오류케이스 수정

--- src/common/filter-builder.ts
+++ src/common/filter-builder.ts
@@ -4,16 +4,19 @@ import { FilterInput } from './graphql/filter-type';
 export function buildWhereClause(filters: FilterInput[]) {
   const whereClause = {};
 
-  filters.forEach((element) => {
-    // between 일 때
-    if (element.operator === 'between') {
-      const betweenDate = element.value.split(',');
-      whereClause[element.field] = {
-        [Op.gte]: betweenDate[0],
-        [Op.lte]: betweenDate[1],
-      };
-    }
-  });
+  // undefined 로 들어오는 케이스가 있음
+  if (filters) {
+    filters.forEach((element) => {
+      // between 일 때
+      if (element.operator === 'between') {
+        const betweenDate = element.value.split(',');
+        whereClause[element.field] = {
+          [Op.gte]: betweenDate[0],
+          [Op.lte]: betweenDate[1],
+        };
+      }
+    });
+  }
 
   return whereClause;
 }
